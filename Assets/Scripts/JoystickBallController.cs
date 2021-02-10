075004d38f5d8e697cba622921b83cd26d00c59a
Marc Zaku <mzaku@hotmail.de>
2021-02-10 09:45:54 +0100

    Adds our own player controller script

---
+++ Assets/Scripts/JoystickBallController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class JoystickBallController : MonoBehaviour {
+	public float speed = 1f;
+	public Joystick joystick;
+	public Rigidbody rb;
+
+	public void FixedUpdate() {
+		var direction = Vector3.forward * this.joystick.Vertical + Vector3.right * this.joystick.Horizontal;
+		this.rb.AddForce(direction * this.speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+	}
+}
