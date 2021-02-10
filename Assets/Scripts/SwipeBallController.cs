365ece37050ed7eaa2e8368e03db80ace591987c
Marc Zaku <mzaku@hotmail.de>
2021-02-10 09:48:55 +0100

    Renames sample scene and adds swipe scene

---
+++ Assets/Scripts/SwipeBallController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SwipeBallController : MonoBehaviour {
+	public float speed = 1f;
+	public Rigidbody rb;
+
+	public void FixedUpdate() {
+		//var direction = Vector3.forward * this.joystick.Vertical + Vector3.right * this.joystick.Horizontal;
+		//this.rb.AddForce(direction * this.speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+	}
+}
