05527bf01682667c222d370ec325aa253066dab2
Marc Zaku <mzaku@hotmail.de>
2021-02-10 10:09:04 +0100

    refactors swipe control script names

--- Assets/Scripts/SwipeBallController.cs
+++ Assets/Scripts/SwipeBallController.cs
@@ -3,19 +3,19 @@
 public class SwipeBallController : MonoBehaviour {
 	public float speed = 1f;
 	public Rigidbody rb;
-	Vector3 startPosition;
-	float startTime;
+	Vector3 screenPositionStart;
+	float timeStart;
 
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
-			this.startPosition = Input.mousePosition;
-			this.startTime = Time.time;
+			this.screenPositionStart = Input.mousePosition;
+			this.timeStart = Time.time;
 		}
 		if (Input.GetMouseButtonUp(0)) {
-			var passedTime = Time.time - this.startTime;
-			var passedDirectionAndDistance = Input.mousePosition - this.startPosition;
-			var direction = Vector3.forward * passedDirectionAndDistance.y + Vector3.right * passedDirectionAndDistance.x;
-			this.rb.AddForce(direction * this.speed / passedTime, ForceMode.VelocityChange);
+			var timeDifference = Time.time - this.timeStart;
+			var screenPositionDifference = Input.mousePosition - this.screenPositionStart;
+			var worldDirection = Vector3.forward * screenPositionDifference.y + Vector3.right * screenPositionDifference.x;
+			this.rb.AddForce(worldDirection * this.speed / timeDifference);
 		}
 	}
 }
