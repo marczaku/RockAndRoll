2ba24d6035a84fa0a390744fbf295425bc98b791
Marc Zaku <mzaku@hotmail.de>
2021-02-11 10:12:09 +0100

    Adds touch ball controller level

--- Assets/Scripts/SwipeBallController.cs
+++ Assets/Scripts/SwipeBallController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class SwipeBallController : MonoBehaviour {
 	public float speed = 1f;
---
+++ Assets/Scripts/TouchBallController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TouchBallController : MonoBehaviour {
+	public float speed = 1f;
+	public Rigidbody rb;
+
+	void FixedUpdate() {
+		if (Input.GetMouseButton(0)) {
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out var hitInfo)) {
+				var direction = (hitInfo.point - this.transform.position).normalized;
+				this.rb.AddForce(direction * this.speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+			}
+		}
+	}
+}
