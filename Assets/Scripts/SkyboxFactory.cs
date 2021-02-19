512d40eac31e28221441b8b8368f4af625931908
Marc Zaku <mzaku@hotmail.de>
2021-02-19 15:41:53 +0100

    skybox changes (playing around with references)

---
+++ Assets/Scripts/SkyboxFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class SkyboxFactory : MonoBehaviour {
+	public GameObject ActiveSkyboxCamera;
+	public AssetReference SkyboxReference;
+	public AssetLabelReference SkyboxLabelReference;
+	void Start() {
+		Destroy(this.ActiveSkyboxCamera);
+		var assetLoadingTask = Addressables.LoadAssetAsync<GameObject>(this.SkyboxReference);
+		assetLoadingTask.Completed += OnSkyboxReady;
+	}
+
+	void OnSkyboxReady(AsyncOperationHandle<GameObject> handle) {
+		this.ActiveSkyboxCamera = Instantiate(handle.Result);
+	}
+}
